84fbc4361c1f0079edc51e42820f785c98ca2de6
vadik.vasiliew2018 <vadik.vasiliew2018@yandex.ru>
2020-06-20 18:45:04 +0300

    Добавьте файлы проекта.

---
+++ HORSE/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace HORSE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.panel1.Location = new System.Drawing.Point(12, 109);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(400, 400);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.panel2.Location = new System.Drawing.Point(418, 109);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(400, 400);
+			this.panel2.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.textBox1.Location = new System.Drawing.Point(12, 28);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.textBox1.Size = new System.Drawing.Size(410, 33);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.CadetBlue;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold);
+			this.button1.Location = new System.Drawing.Point(12, 67);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(140, 34);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Сгенерировать поле";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.CadetBlue;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold);
+			this.button3.Location = new System.Drawing.Point(418, 67);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 32);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Поиск пути";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.label1.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(137, 4);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(158, 19);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Количество обходов";
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.panel3.Location = new System.Drawing.Point(824, 109);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(400, 400);
+			this.panel3.TabIndex = 11;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(428, 35);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(36, 19);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "X = ";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+			this.textBox2.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.textBox2.Location = new System.Drawing.Point(466, 30);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(31, 27);
+			this.textBox2.TabIndex = 14;
+			this.textBox2.Text = "5";
+			this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(501, 35);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(36, 19);
+			this.label4.TabIndex = 15;
+			this.label4.Text = "Y = ";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+			this.textBox3.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.textBox3.Location = new System.Drawing.Point(543, 30);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(31, 27);
+			this.textBox3.TabIndex = 16;
+			this.textBox3.Text = "5";
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(691, 39);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(219, 21);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Прорисовка через каждые";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+			this.textBox4.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.textBox4.Location = new System.Drawing.Point(916, 35);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(64, 27);
+			this.textBox4.TabIndex = 18;
+			this.textBox4.Text = "300";
+			this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(986, 38);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(72, 21);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "обходов";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::HORSE.Properties.Resources.icons8_удалить_16;
+			this.pictureBox1.Location = new System.Drawing.Point(1213, 1);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(22, 22);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 17;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::HORSE.Properties.Resources.HORSE;
+			this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 19;
+			this.pictureBox2.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.ClientSize = new System.Drawing.Size(1235, 566);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Opacity = 0D;
+			this.Text = "Form1";
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        protected System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ HORSE/Form1.cs
@@ -0,0 +1,511 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HORSE
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Get();
+
+
+        }
+
+        public int Col = 0, Row = 0;
+        public ulong bypass = 0;
+
+        Image[] img =
+             {
+                global::HORSE.Properties.Resources._1,
+                global::HORSE.Properties.Resources._2,
+                global::HORSE.Properties.Resources.HORSE5,
+
+            };
+
+        PictureBox[,] Pole;
+
+        List<Point> Pretendent = new List<Point>();
+
+        public int _X_ = 0;
+        public int _Y_ = 0;
+
+        // генерация доски picturebox №1
+        private void Button1_Click(object sender, EventArgs e) { GetPanel(); }
+
+        // Прорисовываем доску графикой
+        public void Dosca()
+        {
+
+
+            bool isBlack = false;
+            Pen pens = new Pen(Color.FromArgb(180, 134, 98));
+            Brush brush = new SolidBrush(Color.FromArgb(180, 134, 98));
+            Pen pensl = new Pen(Color.FromArgb(239, 217, 180));
+            Brush brushal = new SolidBrush(Color.FromArgb(239, 217, 180));
+            Rectangle rect;
+            Graphics gfx = panel2.CreateGraphics();
+
+            for (int i = 0; i <= Row - 1; i++)
+            {
+                for (int j = 0; j <= Col - 1; j++)
+                {
+                    if (isBlack)
+                    {
+                        rect = new Rectangle(i * 40, j * 40, 40, 40);
+                        gfx.DrawRectangle(pens, rect);
+                        gfx.FillRectangle(brush, rect);
+                        isBlack = false;
+                    }
+                    else
+                    {
+                        rect = new Rectangle(i * 40, j * 40, 40, 40);
+                        gfx.DrawRectangle(pensl, rect);
+                        gfx.FillRectangle(brushal, rect);
+                        isBlack = true;
+                    }
+
+                }
+                if (Row % 2 == 0)
+                    isBlack = !isBlack;
+
+            }
+
+            gfx.Dispose();
+            brush.Dispose();
+            brushal.Dispose();
+            pensl.Dispose();
+            pens.Dispose();
+
+        }
+
+        // Соедениям 2 координаты
+        public void Line(int xx, int yy, int XX, int YY)
+        {
+            Point p1 = new Point();
+            Point p2 = new Point();
+            Graphics g = panel2.CreateGraphics();
+            SolidBrush solid = new SolidBrush(Color.Black);
+            Rectangle recth;
+            SolidBrush redBrush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(solid, 1);
+            p1.X = Pole[xx, yy].Location.X + Pole[xx, yy].Size.Width / 2;
+            p1.Y = Pole[xx, yy].Location.Y + Pole[xx, yy].Size.Height / 2;
+            recth = new Rectangle(p1.X - 3, p1.Y - 3, 7, 7);
+            float startAngle = 0.0F;
+            float sweepAngle = 360.0F;
+            g.FillPie(redBrush, recth, startAngle, sweepAngle);
+            p2.X = Pole[XX, YY].Location.X + Pole[XX, YY].Size.Width / 2;
+            p2.Y = Pole[XX, YY].Location.Y + Pole[XX, YY].Size.Height / 2;
+            recth = new Rectangle(p2.X - 3, p2.Y - 3, 7, 7);
+            g.FillPie(redBrush, recth, startAngle, sweepAngle);
+            g.DrawLine(pen, p1, p2);
+            g.Dispose();
+            solid.Dispose();
+            pen.Dispose();
+
+        }
+
+
+        // прорисовки графики
+
+        public int i, j, n, nsqr, q;
+        public int[] dx = new int[9];
+        public int[] dy = new int[9];
+        public int[,] h = new int[29, 29];
+
+        // Доска 3
+        public void DisplayDoscaTree()
+        {
+            bool isBlack = false;
+            Pen pens = new Pen(Color.FromArgb(180, 134, 98));
+            Brush brush = new SolidBrush(Color.FromArgb(180, 134, 98));
+            Pen pensl = new Pen(Color.FromArgb(239, 217, 180));
+            Brush brushal = new SolidBrush(Color.FromArgb(239, 217, 180));
+            Rectangle rect;
+            Graphics gfx = panel3.CreateGraphics();
+
+            for (int i = 0; i <= Row - 1; i++)
+            {
+                for (int j = 0; j <= Col - 1; j++)
+                {
+                    if (isBlack)
+                    {
+                        rect = new Rectangle(i * 40, j * 40, 40, 40);
+                        gfx.DrawRectangle(pens, rect);
+                        gfx.FillRectangle(brush, rect);
+                        isBlack = false;
+                    }
+                    else
+                    {
+                        rect = new Rectangle(i * 40, j * 40, 40, 40);
+                        gfx.DrawRectangle(pensl, rect);
+                        gfx.FillRectangle(brushal, rect);
+                        isBlack = true;
+                    }
+                    //await Task.Delay(1);
+                }
+                if (Row % 2 == 0)
+                    isBlack = !isBlack;
+
+            }
+
+            gfx.Dispose();
+            brush.Dispose();
+            brushal.Dispose();
+            pensl.Dispose();
+            pens.Dispose();
+        }
+
+
+        //третья доска (цифры)
+        async public void DoscaRezult()
+        {
+
+            Graphics gj = panel3.CreateGraphics();
+            int count = 0;
+            int a = _X_ + 1;
+            int b = _Y_ + 1;
+            int kil = h[a, b];
+            Point p1 = new Point();
+            Point p2 = new Point();
+            for (int row = 1; row <= Row; row++)
+            {
+                for (int col = 1; col <= Col; col++)
+                {
+                    if (h[row, col] != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            for (int l = 0; l < count; l++)
+            {
+                for (int ii = 1; ii <= Row; ii++)
+                {
+                    for (int jj = 1; jj <= Col; jj++)
+                    {
+                        if (h[ii, jj] - 1 == kil)
+                        {
+
+                            p1.X = Pole[a - 1, b - 1].Location.X;
+                            p1.Y = Pole[a - 1, b - 1].Location.Y;
+                            p2.X = Pole[ii - 1, jj - 1].Location.X;
+                            p2.Y = Pole[ii - 1, jj - 1].Location.Y;
+                            gj.DrawString(kil.ToString(), new Font("Book Antiqua", 13, FontStyle.Bold), Brushes.Black, p1.X, p1.Y);
+                            await Task.Delay(300);
+                            gj.DrawString(h[ii, jj].ToString(), new Font("Book Antiqua", 13, FontStyle.Bold), Brushes.Black, p2.X, p2.Y);
+
+
+                            kil = h[ii, jj];
+                            a = ii;
+                            b = jj;
+
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+        public ulong counter = 0;
+
+        public int Q1(int q)
+        {
+            if (q == 0)
+                return 1;
+            else
+                return 0;
+
+
+        }
+
+        public int Brute(int i, int x, int y)
+        {
+            int j, u, v, q1 = 0;
+            for (j = 1; j <= 8; j++)
+            {
+                if (Q1(q1) == 0)
+                {
+                    return q1;
+                }
+
+                u = x + dx[j]; // 3
+                v = y + dy[j]; // 2
+                if (1 <= u && u <= n && 1 <= v && v <= n && h[u, v] == 0)
+                {
+                    h[u, v] = i;
+                    if (i < nsqr)
+                    {
+                        q1 = Brute(i + 1, u, v);
+                        if (q1 == 0)
+                        {
+                            h[u, v] = 0;
+                        }
+                    }
+                    else
+                    {
+
+                        q1 = 1;
+                    }
+                }
+            }
+            counter++;
+            if (bypass != 0)
+            {
+                if (counter % bypass == 0)
+                    LineDisplay(h);
+            }
+
+            return q1;
+        }
+
+        //отрисовка линий через n коллчество обходов
+        public void LineDisplay(int[,] h)
+        {
+            int count = 0;
+
+
+            int a = _X_ + 1;
+            int b = _Y_ + 1;
+            int kil = h[a, b];
+            for (int row = 1; row <= Row; row++)
+            {
+                for (int col = 1; col <= Col; col++)
+                {
+                    if ((h[row, col] != 0))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            for (int l = 0; l < count; l++)
+            {
+                for (int ii = 1; ii <= Row; ii++)
+                {
+                    for (int jj = 1; jj <= Col; jj++)
+                    {
+                        if (h[ii, jj] - 1 == kil)
+                        {
+                            Line(a - 1, b - 1, ii - 1, jj - 1);
+                            kil = h[ii, jj];
+                            a = ii;
+                            b = jj;
+
+
+                        }
+                    }
+                }
+            }
+             Dosca();
+           // this.Invoke(new Action(() => { panel2.Refresh(); }));
+            this.Invoke(new Action(() => { textBox1.Text = counter.ToString(); }));
+            
+        }
+
+
+        // метод соеденяющий линиями все координаты
+
+        Point moveStart;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                moveStart = new Point(e.X, e.Y);
+            }
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+
+                Point deltaPos = new Point(e.X - moveStart.X, e.Y - moveStart.Y);
+
+                this.Location = new Point(this.Location.X + deltaPos.X,
+                this.Location.Y + deltaPos.Y);
+            }
+
+        }
+
+
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Поток
+        public void ThreadFunction()
+        {
+
+            Dosca();
+            bypass = Convert.ToUInt64(textBox4.Text);
+            dx[1] = 2; dx[2] = 1; dx[3] = -1; dx[4] = -2;
+            dx[5] = -2; dx[6] = -1; dx[7] = 1; dx[8] = 2;
+            dy[1] = 1; dy[2] = 2; dy[3] = 2; dy[4] = 1;
+            dy[5] = -1; dy[6] = -2; dy[7] = -2; dy[8] = -1;
+            n = Row;
+            nsqr = n * n;
+
+            h[_X_ + 1, _Y_ + 1] = 1;
+
+            int count = 0;
+
+            if (Brute(2, _X_ + 1, _Y_ + 1) == 0)
+                MessageBox.Show("Нет решений");
+
+            else
+            {
+
+               // Dosca();
+                int a = _X_ + 1;
+                int b = _Y_ + 1;
+                int y = h[a, b];
+                for (int row = 1; row <= Row; row++)
+                {
+                    for (int col = 1; col <= Col; col++)
+                    {
+                        if ((h[row, col] != 0))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                for (int l = 0; l < count; l++)
+                {
+                    for (int ii = 1; ii <= Row; ii++)
+                    {
+                        for (int jj = 1; jj <= Col; jj++)
+                        {
+                            if (h[ii, jj] - 1 == y)
+                            {
+                                Line(a - 1, b - 1, ii - 1, jj - 1);
+                                y = h[ii, jj];
+                                a = ii;
+                                b = jj;
+
+                            }
+                        }
+                    }
+                }
+            }
+            this.Invoke(new Action(() => { textBox1.Text = counter.ToString(); }));
+           
+
+            DoscaRezult();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+            DisplayDoscaTree();
+            Thread thread = new Thread(ThreadFunction);
+            thread.Start();
+
+        }
+
+
+        // определение координат по клику
+        //Считывание координат(ставим коня)
+        void Clic(object sender, EventArgs e)
+        {
+
+            PictureBox picClick = sender as PictureBox;
+            Image CurrentPic = picClick.Image;
+
+
+            string[] NamePars = picClick.Name.Split('_');
+            _X_ = Convert.ToInt32(NamePars[1]);
+            _Y_ = Convert.ToInt32(NamePars[2]);
+            Pole[_X_, _Y_].Image = img[2];
+
+
+        }
+
+
+
+        //рисуем доску picturebox
+        void GetPanel()
+        {
+
+            Col = Convert.ToInt32(textBox2.Text);
+            Row = Convert.ToInt32(textBox3.Text);
+            Pole = new PictureBox[Col, Row];
+            int X = 0, Y = 0;
+            panel1.Controls.Clear();
+            bool isBlack = false;
+            for (int xx = 0; xx < Row; xx++)
+            {
+                X = 0;
+                for (int yy = 0; yy < Col; yy++)
+                {
+                    if (isBlack)
+                    {
+                        PictureBox pic = new PictureBox()
+
+                        {
+                            Image = img[0],
+                            Location = new System.Drawing.Point(X, Y),
+                            Name = "_" + xx + "_" + yy,
+                            Size = new System.Drawing.Size(40, 40),
+                            SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+                        };
+
+                        pic.Click += Clic;
+                        Pole[xx, yy] = pic;
+                        panel1.Controls.Add(pic);
+                        X += 40;
+                        isBlack = false;
+                    }
+                    else
+                    {
+                        PictureBox pic = new PictureBox()
+
+                        {
+                            Image = img[1],
+                            Location = new System.Drawing.Point(X, Y),
+                            Name = "_" + xx + "_" + yy,
+                            Size = new System.Drawing.Size(40, 40),
+                            SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+                        };
+
+                        pic.Click += Clic;
+                        Pole[xx, yy] = pic;
+                        panel1.Controls.Add(pic);
+                        X += 40;
+                        isBlack = true;
+                    }
+
+                }
+                Y += 40;
+                if (Row % 2 == 0)
+                    isBlack = !isBlack;
+            }
+        }
+
+        async public void Get()
+        {
+            for (double i = 0.0; i < 0.95 ; i+=0.05)
+            {
+                this.Opacity = i;
+                await Task.Delay(20);
+            }
+        }
+    }
+}
